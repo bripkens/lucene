a9eaccff89b8ac522203e49f8d4522aacdf9e127
Ben Ripkens <bripkens@gmail.com>
2017-01-09 21:43:21 +0100

    Support query AST stringification

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,8 @@
 # Changelog
 
+## Unreleased
+ - Turn ASTs into queries via `lucene.toString(ast)`.
+ - Retain information about quoted strings in the AST.
+
 ## 1.0.0
  - Initial release
---
+++ test/toString_test.js
@@ -0,0 +1,51 @@
+const expect = require('chai').expect;
+
+const lucene = require('../');
+
+describe('toString', () => {
+  it('must handle empty ast', () => {
+    testAst(null, '');
+    testAst(undefined, '');
+    testAst(null, '');
+  });
+
+  it('must handle simple terms', () => {
+    testStr('bar');
+  });
+
+  it('must handle quoted terms', () => {
+    testStr('"fizz buz"');
+  });
+
+  it('must support field groups', () => {
+    testStr('foo:(bar OR baz)');
+  });
+
+  it('must support fuzzy', () => {
+    testStr('foo~0.6');
+  });
+
+  it('must support fuzzy without explicit similarity', () => {
+    testStr('foo~');
+  });
+
+  it('must drop default similarity value', () => {
+    testStr('foo~0.5', 'foo~');
+  });
+
+  it('must support terms with \'-\'', () => {
+    testStr('created_at:>now-5d');
+  });
+
+  it('must support terms with \'+\'', () => {
+    testStr('created_at:>now+5d');
+  });
+
+  function testAst(ast, expected) {
+    expect(lucene.toString(ast)).to.equal(expected);
+  }
+
+  function testStr(str, expected) {
+    expect(lucene.toString(lucene.parse(str))).to.equal(expected || str);
+  }
+});
