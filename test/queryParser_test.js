f0f3948ba88b205f91f613a8b005064903a2acf3
fanlia <fanlia@outlook.com>
2017-11-24 17:11:43 +0800

    feature-support-lowercase-conjunction-operators

--- test/queryParser_test.js
+++ test/queryParser_test.js
@@ -208,6 +208,30 @@ describe('queryParser', () => {
       expect(results['right']['term']).to.equal('buzz');
     });
 
+    it('parses explicit conjunction operator (and)', () => {
+      var results = lucene.parse('fizz and buzz');
+
+      expect(results['left']['term']).to.equal('fizz');
+      expect(results['operator']).to.equal('AND');
+      expect(results['right']['term']).to.equal('buzz');
+    });
+
+    it('parses explicit conjunction operator (or)', () => {
+      var results = lucene.parse('fizz or buzz');
+
+      expect(results['left']['term']).to.equal('fizz');
+      expect(results['operator']).to.equal('OR');
+      expect(results['right']['term']).to.equal('buzz');
+    });
+
+    it('parses explicit conjunction operator (not)', () => {
+      var results = lucene.parse('fizz not buzz');
+
+      expect(results['left']['term']).to.equal('fizz');
+      expect(results['operator']).to.equal('NOT');
+      expect(results['right']['term']).to.equal('buzz');
+    });
+
     it('parses explicit conjunction operator (&&)', () => {
       var results = lucene.parse('fizz && buzz');
 
