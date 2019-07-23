f6e7f12fc5635bec735ba9509ba64ef73f5c5683
Ben Ripkens <bripkens@gmail.com>
2019-07-23 14:41:05 +0200

    Update dependencies to resolve security vulnerabilities in dev dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,7 @@ module.exports = {
     indent: ['error', 2],
     'linebreak-style': ['error', 'unix'],
     quotes: ['error', 'single'],
-    semi: ['error', 'always']
+    semi: ['error', 'always'],
+    'no-useless-escape': 0
   }
 };
